bfd9ff43ef4dd077958a754985d1fc8882864f62
Kirill Chizhov <sstive39@gmail.com>
2022-09-25 01:18:12 +0300

    add submodule

--- States/init_state.py
+++ States/init_state.py
@@ -2,19 +2,23 @@ from loguru import logger
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from Enums import UserData
+import Enums
 from .base_state import BaseState
+
+from Word4Univer.
 
 
 class InitState(BaseState):
 
     states = ["get_fullname", "get_group", "get_variant"]
 
-    def __init__(self, step: int = 0, update: Update = None, context: ContextTypes.DEFAULT_TYPE = None):
-        self.step = step
+    def __init__(self, update: Update = None, context: ContextTypes.DEFAULT_TYPE = None):
+        self.step = 0
 
-        self.update = update
-        self.context = context
+        super().__init__(update, context)
+
+    def start(self):
+        self.context.user_data[Enums.UserData.StudentInfo] = ""
 
     def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         self.update = update
@@ -24,6 +28,9 @@ class InitState(BaseState):
         self.update.message.reply_text(
             "Напиши свое полное имя (ФИО), которое будет написано в титульнике:"
         )
+
+    def __save_fullname(self):
+        pass
 
     def __request_group(self) -> None:
         self.update.message.reply_text(
@@ -36,15 +43,20 @@ class InitState(BaseState):
         )
 
     def __finale(self) -> None:
-        student = self.context.user_data.get(UserData.StudentInfo)
+        student = self.context.user_data.get(Enums.UserData.StudentInfo)
 
-        if info is None:
+        if student is None:
             logger.critical(f"Student info is null! user data: {self.context.user_data}")
             self.update.message.reply_text("Произошла критическая ошибка!")
             return
 
+        # TODO
         self.update.message.reply_text(
             "Все, теперь я запомнил твои данные и их не надо будет каждый раз вводить заново.\n"
             "Давай проверим:\n"
-            f"ФИО: {}"
+            f"ФИО: {student.name}\n"
+            f"Группа: {student.group}\n"
+            f"Вариант: №{student.variant}\n"
+            f"\n"
+            f"Если данные введены не правильно, то их можно исправить командой /{Enums.Command.EditData}"
         )
